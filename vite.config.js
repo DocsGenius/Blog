747d85aeabecabb90fbcc712e0128042e1a31b8c
Namoruno <ramenrand@gmail.com>
2026-02-12 06:40:24 -0500

    Fix build process - keep script tag for Vite entry point detection

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,5 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
   }
 })
