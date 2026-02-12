744d9d0f8dc91bde17e3000555b01f379e04a099
Namoruno <ramenrand@gmail.com>
2026-02-12 05:54:44 -0500

    switch routing types again gh-pages fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo, useCallback, memo } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import './styles/article.css'
 import './themes/theme-variables.css'
