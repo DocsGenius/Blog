8bdcd42e9147b06b872dab80f936d9c4066ec184
Namoruno <ramenrand@gmail.com>
2026-02-12 06:23:14 -0500

    Updates

---
+++ assets/HelloGenius.qYb_YqVm.js
@@ -0,0 +1,36 @@
+const e=`---
+title: Hello Genius!
+subtitle: This article goes over everything Docs Genius!
+authorId: roman-rand
+date: February 12th, 2026
+category: Newsletter
+readingTime: 1 min read
+coverImage: /branding/DocsGeniusIcon.png
+tags: ['Technology', 'Writing', 'General', 'DocsGenius']
+---
+
+DocsGenius is a blog started By Roman Rand.
+Later on, DocsGenius plans on exclusively supporting small writers and students.
+
+## #Status Update
+
+The website is currently in development. For those so lucky to be reading this, I made some fun themes for you all at the bottom of the page!
+
+\`\`\`js
+haveFun(true);
+\`\`\`
+
+## A glance into the future
+
+Docs Genius plans on supporting exclusively small writers, undergraduates/masters, and self-taught developers.
+The discord is under works, but will be the subject of the next *daily* update
+
+\`\`\`js
+empowerStudents(always);
+\`\`\`
+
+## How can you help?
+
+Keep your eyes peeled for the next update, it might include a link to our discord.
+We're going to need all hands on deck, see you around!
+`;export{e as default};
