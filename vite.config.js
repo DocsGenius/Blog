c65f85f1247d0dd58f52e44e1c12a45f6c20ad5e
Namoruno <ramenrand@gmail.com>
2026-02-12 06:25:49 -0500

    setup actions

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,16 @@ export default defineConfig({
     react()
   ],
   base: '/Blog/', // important for GitHub Pages project site
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
